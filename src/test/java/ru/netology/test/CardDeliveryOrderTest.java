e5d6502b17b5b9a2e3af1c4c13d08b278b8ddfb8
BelyakovArkadiy <belyakovarkadiy@gmail.com>
2021-03-23 19:56:22 +0600

    modified:   build.gradle 	modified:   src/test/java/ru/netology/test/CardDeliveryOrderTest.java

--- src/test/java/ru/netology/test/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/test/CardDeliveryOrderTest.java
@@ -1,6 +1,10 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -16,11 +20,19 @@ import static com.codeborne.selenide.Selenide.open;
 public class CardDeliveryOrderTest {
 
 
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
     @BeforeEach
     public void setUp() {
         open("http://localhost:9999");
     }
-
 
     @Test
     void shouldTestAllFields() {
